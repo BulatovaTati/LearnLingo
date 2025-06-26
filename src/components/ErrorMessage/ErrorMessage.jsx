6bd838cceb333d243c2aa26e8c9ca1034c88184d
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-06-26 15:33:36 +0300

    Correct image path

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import s from './ErrorMessage.module.css';
 const ErrorMessage = ({ message }) => {
     return (
         <>
-            <img className={s.image} src="../../../public/images/sad-girl.png" alt="Sad Girl" />
+            <img className={s.image} src="/images/sad-girl.png" alt="Sad Girl" />
             <p className={s.errorMessage}>{message}</p>
         </>
     );
